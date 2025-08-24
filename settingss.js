bdcacd03eb2ef42309bc4f6b9e88857692d0cb2c
Malvin King <malvintech2@gmail.com>
2025-08-24 10:00:57 +0200

    Create settingss.js

---
+++ settingss.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  SESSION_ID: "",  // add your Session Id here
+  
+  OWNER_NUMBER: "263714757857", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+};
